139597910bd10a755210ff77237543a23962ce65
volcain-io <volcain.io@gmail.com>
2018-10-17 13:50:40 +0200

    Initial commit

---
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,12 @@
+export function steps(num) {
+  if (num <= 0) throw new Error("Only positive numbers are allowed");
+
+  let count = 0;
+
+  while (num > 1) {
+    num = num % 2 ? 3 * num + 1 : num / 2;
+    count++;
+  }
+
+  return count;
+}
