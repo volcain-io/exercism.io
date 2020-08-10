db70e9c491b7787e1e3eb38f50339e0311dfb3ac
volcain-io <volcain.io@gmail.com>
2020-08-10 18:59:30 +0200

    Initial commit

---
+++ bash/error-handling/error_handling.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+main() {
+  name=$1
+
+  if [[ "$#" -ne 1 ]]; then
+    echo "Usage: error_handling.sh <person>"
+    exit 1
+  else
+    echo "Hello, ${name}"
+  fi
+
+  exit 0
+}
+
+main "$@"
