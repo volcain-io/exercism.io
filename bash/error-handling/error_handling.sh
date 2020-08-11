8a949e7a97336f52d29a3eff91fda14111e2ed5c
volcain-io <volcain.io@gmail.com>
2020-08-11 09:51:44 +0200

    Refactor code
    Use ((...)) for arithmetic operations.
    Quote variable expansions.
    Use return instead of exit in functions.

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -3,13 +3,13 @@
 main() {
   name=$1
 
-  if [ $# -ne 1 ]; then
+  if (( "$#" != 1 )); then
     echo "Usage: error_handling.sh <person>"
     return 1
   fi
 
   echo "Hello, ${name}"
-  exit $?
+  return "$?"
 }
 
 main "$@"
