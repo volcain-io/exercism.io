820475209accd54157bfd2ec49b773f8dafa7352
volcain-io <volcain.io@gmail.com>
2020-08-13 13:08:07 +0200

    Test after argument check

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 main() {
-  name="$1"
-
   if (( "$#" != 1 )); then
     echo "Usage: error_handling.sh <person>"
     return 1
   fi
 
-  echo "Hello, ${name}"
+  echo "Hello, ${1}"
 }
 
 main "$@"
