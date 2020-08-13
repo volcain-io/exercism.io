fda1aad6628c95fa97391259c09a25ff56a525c4
volcain-io <volcain.io@gmail.com>
2020-08-13 17:00:31 +0200

    Initial commit

---
+++ bash/hamming/hamming.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+main () {
+  (( "$#" != 2 )) && {
+    echo "Usage: hamming.sh <string1> <string2>"
+    return 1
+  }
+
+  local string1="${1}"
+  local string2="${2}"
+
+  (( ${#string1} == ${#string2} )) && {
+    [[ "${string1}" != "${string2}" ]] &&
+      for (( i=0; i<${#string1}; i++ )); do
+        [[ "${string1:$i:1}" != "${string2:$i:1}" ]] && diff+=${string2:$i:1}
+      done
+    echo "${#diff}"
+    return 0
+  }
+
+  echo "left and right strands must be of equal length"
+  return 1
+}
+
+main "$@"
