55e053d301ae22eb8d3f2b186bff6ccbadff0abb
volcain-io <volcain.io@gmail.com>
2020-08-15 13:18:41 +0200

    Left-align happy path

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,20 +1,27 @@
+// Package hamming provides a simple implementation of the
+// differences between two DNA strands
 package hamming
 
 import "errors"
 
+// Distance counts the differences between two DNA strands (Hamming Distance).
+// Returns an error, if one of the DNA strands is of different length.
+// Returns the number of differences.
 func Distance(a, b string) (int, error) {
-	result := 0
-
-	if len(a) == len(b) {
-		if a != b {
-			for i := 0; i < len(a); i++ {
-				if a[i] != b[i] {
-					result += 1
-				}
-			}
-		}
-		return result, nil
+	if len(a) != len(b) {
+		return 0, errors.New("left and right strands must be of equal length")
 	}
 
-	return result, errors.New("left and right strands must be of equal length")
+	if a == b {
+		return 0, nil
+	}
+
+	diffCounter := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			diffCounter++
+		}
+	}
+
+	return diffCounter, nil
 }
