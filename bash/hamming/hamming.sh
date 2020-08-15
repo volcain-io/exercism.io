62a09c99cbf162b71eb12ea548635000b9b98e5c
volcain-io <volcain.io@gmail.com>
2020-08-15 13:24:45 +0200

    Left-align happy path

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -9,17 +9,21 @@ main () {
   local string1="${1}"
   local string2="${2}"
 
-  (( ${#string1} == ${#string2} )) && {
-    [[ "${string1}" != "${string2}" ]] &&
-      for (( i=0; i<${#string1}; i++ )); do
-        [[ "${string1:$i:1}" != "${string2:$i:1}" ]] && diff+=${string2:$i:1}
-      done
-    echo "${#diff}"
+  (( ${#string1} != ${#string2} )) && {
+    echo "left and right strands must be of equal length"
+    return 1
+  }
+
+  [[ "${string1}" == "${string2}" ]] && {
+    echo "0"
     return 0
   }
 
-  echo "left and right strands must be of equal length"
-  return 1
+  for (( i=0; i<${#string1}; i++ )); do
+    [[ "${string1:$i:1}" != "${string2:$i:1}" ]] && diff+=${string2:$i:1}
+  done
+
+  echo "${#diff}"
 }
 
 main "$@"
