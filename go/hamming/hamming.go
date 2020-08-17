20a29264f380330b0c8ad3960a6c746e1d40f62d
volcain-io <volcain.io@gmail.com>
2020-08-17 10:42:42 +0200

    Make comparison UTF-8 compatible

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,19 +2,25 @@
 // differences between two DNA strands
 package hamming
 
-import "errors"
+import (
+	"errors"
+	"unicode/utf8"
+)
 
 // Distance counts the differences between two DNA strands (Hamming Distance).
 // Returns an error, if one of the DNA strands is of different length.
 // Returns the number of differences.
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
+	if utf8.RuneCountInString(a) != utf8.RuneCountInString(b) {
 		return 0, errors.New("left and right strands must be of equal length")
 	}
 
+	runeA := []rune(a)
+	runeB := []rune(b)
+
 	diffCounter := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
+	for i := 0; i < len(runeA); i++ {
+		if runeA[i] != runeB[i] {
 			diffCounter++
 		}
 	}
