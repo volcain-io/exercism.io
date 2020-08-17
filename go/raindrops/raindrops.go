6758ae54e7230097320926be44d5144bc415a05d
volcain-io <volcain.io@gmail.com>
2020-08-17 16:27:10 +0200

    Initial commit

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,28 @@
+// Package raindrops provides a simple implementation of the
+// famous 'FizzBuzz' problem
+package raindrops
+
+import "strconv"
+
+// Convert converts a given number into a string containing raindrops
+// sounds corresponding to ceratin potential factors
+// Returns the raindrops string
+func Convert(number int) string {
+	var result string
+
+	if number%3 == 0 {
+		result = "Pling"
+	}
+	if number%5 == 0 {
+		result += "Plang"
+	}
+	if number%7 == 0 {
+		result += "Plong"
+	}
+
+	if len(result) == 0 {
+		result = strconv.Itoa(number)
+	}
+
+	return result
+}
