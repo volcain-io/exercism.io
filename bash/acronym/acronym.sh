e9a8abdf95000fa990869b0f99e8083f38812682
volcain-io <volcain.io@gmail.com>
2020-08-18 08:36:10 +0200

    Refactor code

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
+trim_quotes() {
+  # Usage: trim_quotes "string"
+  : "${1//\'}"
+  echo -n "${_//\"}"
+}
+
+replace_all() {
+  # Usage: replace_all "string" "pattern" "replacement"
+  echo -n "${1//$2/$3}"
+}
+
 main () {
   if [[ "$#" -eq 0 ]]; then
     echo "Usage: acronym.sh <long name>"
     return 1
   fi
 
-  replace_hiphen="${1//-/ }"
-  alpha_only="${replace_hiphen//[[:punct:]]/}"
-  read -r -a words <<< "${alpha_only}"
+  no_quotes="$(trim_quotes "$1")"
+  clean_string="$(replace_all "${no_quotes}" "[[:punct:]]" " ")"
+
+  read -r -a words <<< "${clean_string}"
 
   for word in "${words[@]}"; do
     acronym+="${word:0:1}"
