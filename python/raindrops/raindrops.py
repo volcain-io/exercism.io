88d3e6d656632b9eed589b3fcbba1c9c9d657cdf
volcain-io <volcain.io@gmail.com>
2020-08-23 14:20:58 +0200

    Add comments & ternary operator

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,16 +1,47 @@
+"""Raindrops exercise from exercism.io python path.
+
+Convert a number into a string that contains raindrop sounds corresponding to
+certain potential factors. A factor is a number that evenly divides into
+another number, leaving no remainder. The simplest way to test if a one number
+is a factor of another is to use the modulo operation.
+
+The rules of raindrops are that if a given number:
+
+    - has 3 as a factor, add 'Pling' to the result.
+    - has 5 as a factor, add 'Plang' to the result.
+    - has 7 as a factor, add 'Plong' to the result.
+    - does not have any of 3, 5, or 7 as a factor, the result should be the
+      digits of the number.
+"""
+
+
 def convert(number):
-    result = ""
+    """Convert number into raindrop string.
+
+    Convert a number into a string that contains raindrop
+    sounds corresponding to certain potential factors.
+
+    Args:
+        number: Any integer
+
+    Returns:
+        A string that contains raindrop sounds corresponding
+        to certain potential factors. For example:
+
+        - 28 has 7 as a factor, but not 3 or 5, so the result would be "Plong".
+        - 30 has both 3 and 5 as factors, but not 7, so the result would be
+          "PlingPlang".
+        - 34 is not factored by 3, 5, or 7, so the result would be "34".
+    """
+    result = ''
 
     if number % 3 == 0:
-        result = "Pling"
+        result = 'Pling'
 
     if number % 5 == 0:
-        result += "Plang"
+        result += 'Plang'
 
     if number % 7 == 0:
-        result += "Plong"
+        result += 'Plong'
 
-    if len(result) == 0:
-        result = str(number)
-
-    return result
+    return result if result else str(number)
