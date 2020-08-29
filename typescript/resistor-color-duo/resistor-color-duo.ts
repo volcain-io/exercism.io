afe93b04d2c1de11435d195e86ebf0f98ba3f6c6
volcain-io <volcain.io@gmail.com>
2020-08-29 09:39:33 +0200

    Use String Literal Types

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,31 +1,33 @@
+type Color = "black" | "brown" | "red" | "orange" | "yellow" | "green" | "blue" | "violet" | "grey" | "white";
+
 export class ResistorColor {
-  private colors: string[];
-  private bandColors: string[];
+    private colors: Color[];
+    private bandColors: Color[];
 
-  constructor(colors: string[]) {
-    if (colors.length < 2) {
-      throw new Error("At least two colors need to be present");
+    constructor(colors: Color[]) {
+        if (colors.length < 2) {
+            throw new Error("At least two colors need to be present");
+        }
+        this.colors = colors;
+        this.bandColors = [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        ];
     }
-    this.colors = colors;
-    this.bandColors = [
-      "black",
-      "brown",
-      "red",
-      "orange",
-      "yellow",
-      "green",
-      "blue",
-      "violet",
-      "grey",
-      "white",
-    ];
-  }
 
-  value = (): number =>
-    Number(
-      this.colors
-        .slice(0, 2)
-        .map((color) => this.bandColors.indexOf(color))
-        .join("")
-    );
+    value = (): number =>
+        Number(
+            this.colors
+                .slice(0, 2)
+                .map((color) => this.bandColors.indexOf(color))
+                .join("")
+        );
 }
