234e096f36a0b1f4bef33012dad6d76a422efad3
volcain-io <volcain.io@gmail.com>
2020-09-01 12:30:35 +0200

    Use search & replace

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,31 +1,7 @@
 export function encode(param) {
-  let encoding = '';
-
-  if (param) {
-    param.match(/(.)\1*/g).forEach((fullMatch) => {
-      const len = fullMatch.length;
-      const ch = fullMatch[0];
-      encoding += len === 1 ? ch : `${len + ch}`;
-    });
-  }
-
-  return encoding;
+  return param.replace(/(.)\1+/g, (match, letter) => match.length + letter);
 }
 
 export function decode(param) {
-  let result = '';
-  let tmp = '';
-
-  if (param) {
-    param.split('').forEach((ch) => {
-      if (Number(ch)) {
-        tmp += `${ch}`;
-      } else {
-        result += tmp ? ch.repeat(Number(tmp)) : ch;
-        tmp = '';
-      }
-    });
-  }
-
-  return result;
+  return param.replace(/(\d+)(.)/g, (_, count, letter) => letter.repeat(count));
 }
