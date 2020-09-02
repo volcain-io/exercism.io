5cc218dedc3ad71a9df63d0b55f4e33ab49a8a1d
volcain-io <volcain.io@gmail.com>
2020-09-02 16:41:08 +0200

    Refactor whole code

--- bash/space-age/space_age.sh
+++ bash/space-age/space_age.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
-main () {
-  planet=${1:-"Earth"}
+declare -r EARTH_YEAR_IN_SECONDS=31557600
+declare -rA PLANET_EARTH_YEARS=( [Earth]=1 [Jupiter]=11.862615 [Mars]=1.8808158 [Mercury]=0.2408467 [Neptune]=164.79132 [Saturn]=29.447498 [Uranus]=84.016846 [Venus]=0.61519726)
+
+calculate_age() {
+  # Usage: calculate_age <planet> <seconds>
+  planet="${1}"
   seconds="${2}"
-  earth_years=1
 
-  case ${planet} in
-    "Earth") earth_years=1;;
-    "Jupiter") earth_years=11.862615;;
-    "Mars") earth_years=1.8808158;;
-    "Mercury") earth_years=0.2408467;;
-    "Neptune") earth_years=164.79132;;
-    "Saturn") earth_years=29.447498;;
-    "Uranus") earth_years=84.016846;;
-    "Venus") earth_years=0.61519726;;
-    *) echo "not a planet"; exit 1;;
-  esac
+  awk "BEGIN { printf \"%.2f\",${seconds}/${EARTH_YEAR_IN_SECONDS}/${PLANET_EARTH_YEARS[${planet}]};exit(0)}"
+}
 
-  awk "BEGIN { printf \"%.2f\",${seconds}/31557600/${earth_years};exit(0)}"
+main () {
+  if [[ "$#" != 2 ]]; then
+    echo "Usage: space-age.sh <planet> <seconds>"
+  fi
+
+  if ! [[ "${PLANET_EARTH_YEARS[${1}]}" ]]; then
+    echo "not a planet"
+    return 1
+  fi
+
+  calculate_age "${1}" "${2}"
 }
 
 main "$@"
