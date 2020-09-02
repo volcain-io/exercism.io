3493bebca126f68cf2cf08f0ba03ca4384fe33fc
volcain-io <volcain.io@gmail.com>
2020-09-02 16:48:09 +0200

    Format code

--- bash/space-age/space_age.sh
+++ bash/space-age/space_age.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 declare -ri EARTH_YEAR_IN_SECONDS=31557600
-declare -rA PLANET_EARTH_YEARS=( [Earth]=1 [Jupiter]=11.862615 [Mars]=1.8808158 [Mercury]=0.2408467 [Neptune]=164.79132 [Saturn]=29.447498 [Uranus]=84.016846 [Venus]=0.61519726)
+declare -rA PLANET_EARTH_YEARS=(
+  [Earth]=1
+  [Jupiter]=11.862615
+  [Mars]=1.8808158
+  [Mercury]=0.2408467
+  [Neptune]=164.79132
+  [Saturn]=29.447498
+  [Uranus]=84.016846
+  [Venus]=0.61519726
+)
 
 main () {
   if [[ "$#" != 2 ]]; then
