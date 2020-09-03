daa019740fd2560fadbea2d99bc52106a525647a
volcain-io <volcain.io@gmail.com>
2020-09-03 09:24:34 +0200

    Make code readable

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -3,17 +3,26 @@
 main () {
   if [[ "$#" != 1 ]]; then
     echo "Usage: scrabble-score.sh <word>"
+    return 1
   fi
 
-  local -l lowercase="${1}"
-  local -A score_table=( [a]=1 [b]=3 [c]=3 [d]=2 [e]=1 [f]=4 [g]=2 [h]=4 [i]=1 [j]=8 [k]=5 [l]=1 [m]=3 [n]=1 [o]=1 [p]=3 [q]=10 [r]=1 [s]=1 [t]=1 [u]=1 [v]=4 [w]=4 [x]=8 [y]=4 [z]=10 )
+  local -rl lowercase="${1}"
+  local -rA score_table=(
+    [a]=1 [b]=3 [c]=3
+    [d]=2 [e]=1 [f]=4
+    [g]=2 [h]=4 [i]=1
+    [j]=8 [k]=5 [l]=1
+    [m]=3 [n]=1 [o]=1
+    [p]=3 [q]=10 [r]=1
+    [s]=1 [t]=1 [u]=1
+    [v]=4 [w]=4 [x]=8
+    [y]=4 [z]=10
+  )
   local -i count=0
 
   for (( i = 0; i < "${#lowercase}"; i++ )); do
     ch=${lowercase:i:1}
-    if [[ "${ch}" =~ [a-z] ]]; then
-      (( count+="${score_table[$ch]}" ))
-    fi
+    (( count+="${score_table[$ch]}" ))
   done
 
   echo "${count}"
