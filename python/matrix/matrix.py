8453c6ecb3a1b1a5b77ad1ac839923b7aa62c932
volcain-io <volcain.io@gmail.com>
2020-09-04 15:37:43 +0200

    Initial commit

---
+++ python/matrix/matrix.py
@@ -0,0 +1,60 @@
+"""Matrix exercise from exercism.io python path.
+
+Given a string representing a matrix of numbers,
+return the rows and columns of that matrix.
+"""
+
+
+class Matrix:
+    """Matrix class
+
+    Given a string representing a matrix of numbers,
+    return the rows and columns of that matrix.
+    """
+    def __init__(self, matrix_string):
+        self.matrix_array = self.to_array(matrix_string)
+
+    def row(self, index):
+        """Get row values by its index.
+
+        Args:
+            index: positive integer.
+
+        Returns:
+            An array of integers representing the row values.
+        """
+        return self.matrix_array[index - 1]
+
+    def column(self, index):
+        """Get column values by its index.
+
+        Args:
+            index: positive integer.
+
+        Returns:
+            An array of integers representing the column values.
+        """
+        columns = []
+        for row in self.matrix_array:
+            columns.append(row[index - 1])
+        return columns
+
+    @classmethod
+    def to_array(cls, matrix_string):
+        """Get multi-dimensional array
+
+        Args:
+            string: string representing the matrix
+
+        Returns:
+            A multi-dimensional array representing the matrix.
+        """
+        matrix_array = []
+        plain_rows = matrix_string.split('\n')
+        for row in plain_rows:
+            rows = []
+            values = row.split(' ')
+            for value in values:
+                rows.append(int(value))
+            matrix_array.append(rows)
+        return matrix_array
