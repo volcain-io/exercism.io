767eae522d443922527fafa668f9bb6fd0d7ccef
volcain-io <volcain.io@gmail.com>
2020-09-06 13:01:02 +0200

    Use recursion

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -3,44 +3,43 @@
 package scrabble
 
 import (
-	"strings"
+	"unicode"
 )
+
+var scoreTable = map[rune]int{
+	'A': 1,
+	'B': 3,
+	'C': 3,
+	'D': 2,
+	'E': 1,
+	'F': 4,
+	'G': 2,
+	'H': 4,
+	'I': 1,
+	'J': 8,
+	'K': 5,
+	'L': 1,
+	'M': 3,
+	'N': 1,
+	'O': 1,
+	'P': 3,
+	'Q': 10,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'U': 1,
+	'V': 4,
+	'W': 4,
+	'X': 8,
+	'Y': 4,
+	'Z': 10,
+}
 
 // Compute the Scrabble score of the given word
 func Score(word string) int {
-	scoreTable := map[string]int{
-		"A": 1,
-		"B": 3,
-		"C": 3,
-		"D": 2,
-		"E": 1,
-		"F": 4,
-		"G": 2,
-		"H": 4,
-		"I": 1,
-		"J": 8,
-		"K": 5,
-		"L": 1,
-		"M": 3,
-		"N": 1,
-		"O": 1,
-		"P": 3,
-		"Q": 10,
-		"R": 1,
-		"S": 1,
-		"T": 1,
-		"U": 1,
-		"V": 4,
-		"W": 4,
-		"X": 8,
-		"Y": 4,
-		"Z": 10,
+	runeWord := []rune(word)
+	if len(runeWord) == 0 {
+		return 0
 	}
-
-	sum := 0
-	for letter, score := range scoreTable {
-		sum += strings.Count(strings.ToUpper(word), letter) * score
-	}
-
-	return sum
+	return Score(word[1:]) + scoreTable[unicode.ToUpper(runeWord[0])]
 }
