ab6c14570d146cb85d9a75d47336e891e3c5b9cc
volcain-io <volcain.io@gmail.com>
2020-09-06 13:17:50 +0200

    Use recursion

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,36 +1,37 @@
+Map<String, int> scoreTable = Map.unmodifiable(<String, int>{
+  'A': 1,
+  'B': 3,
+  'C': 3,
+  'D': 2,
+  'E': 1,
+  'F': 4,
+  'G': 2,
+  'H': 4,
+  'I': 1,
+  'J': 8,
+  'K': 5,
+  'L': 1,
+  'M': 3,
+  'N': 1,
+  'O': 1,
+  'P': 3,
+  'Q': 10,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'U': 1,
+  'V': 4,
+  'W': 4,
+  'X': 8,
+  'Y': 4,
+  'Z': 10
+});
+
 int score([final String word = '']) {
-  Map<String, int> scoreTable = Map.unmodifiable(<String, int>{
-    'A': 1,
-    'B': 3,
-    'C': 3,
-    'D': 2,
-    'E': 1,
-    'F': 4,
-    'G': 2,
-    'H': 4,
-    'I': 1,
-    'J': 8,
-    'K': 5,
-    'L': 1,
-    'M': 3,
-    'N': 1,
-    'O': 1,
-    'P': 3,
-    'Q': 10,
-    'R': 1,
-    'S': 1,
-    'T': 1,
-    'U': 1,
-    'V': 4,
-    'W': 4,
-    'X': 8,
-    'Y': 4,
-    'Z': 10
-  });
+  if (word.isEmpty) {
+    return 0;
+  }
 
-  int sum = word.toUpperCase().split('').fold<int>(0, (curr, next) {
-    return curr + scoreTable[next];
-  });
-
-  return sum;
+  return score(word.substring(1)) +
+      scoreTable[word.substring(0, 1).toUpperCase()];
 }
