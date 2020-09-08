1814fc15234205a7f34c67560aa9b0e477c6c17c
volcain-io <volcain.io@gmail.com>
2020-09-08 14:21:26 +0200

    Use switch-statement instead of a associative array for lookup

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-declare -rA score_table=(
-  [a]=1 [b]=3 [c]=3
-  [d]=2 [e]=1 [f]=4
-  [g]=2 [h]=4 [i]=1
-  [j]=8 [k]=5 [l]=1
-  [m]=3 [n]=1 [o]=1
-  [p]=3 [q]=10 [r]=1
-  [s]=1 [t]=1 [u]=1
-  [v]=4 [w]=4 [x]=8
-  [y]=4 [z]=10
-)
-
 main () {
   if [[ "$#" != 1 ]]; then
     echo "Usage: scrabble-score.sh <word>"
@@ -19,14 +7,37 @@ main () {
   fi
 
   local -rl lowercase="${1}"
-  local -i count=0
+  local -i sum=0
 
   for (( i = 0; i < "${#lowercase}"; i++ )); do
-    ch=${lowercase:i:1}
-    (( count+="${score_table[$ch]:-0}" ))
+    case ${lowercase:i:1} in
+      'a'|'e'|'i'|'l'|'n'|'o'|'r'|'s'|'t'|'u')
+        (( sum+=1 ))
+        ;;
+      'd'|'g')
+        (( sum+=2 ))
+        ;;
+      'b'|'c'|'m'|'p')
+        (( sum+=3 ))
+        ;;
+      'f'|'h'|'v'|'w'|'y')
+        (( sum+=4 ))
+        ;;
+      'k')
+        (( sum+=5 ))
+        ;;
+      'j'|'x')
+        (( sum+=8 ))
+        ;;
+      'q'|'z')
+        (( sum+=10 ))
+        ;;
+      *)
+        (( sum+=0 ))
+    esac
   done
 
-  echo "${count}"
+  echo "${sum}"
 }
 
 main "$@"
