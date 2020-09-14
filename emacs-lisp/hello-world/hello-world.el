5c150e35f992979f18426b3d4fac959e4492aa4c
volcain-io <volcain.io@gmail.com>
2020-09-14 14:03:37 +0200

    Initial commit

---
+++ emacs-lisp/hello-world/hello-world.el
@@ -0,0 +1,12 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+;; The classical introductory exercise. Just say "Hello, World!"
+
+;;; Code:
+
+(defun hello ()
+  (message "Hello, World!"))
+
+(provide 'hello-world)
+;;; hello-world.el ends here
