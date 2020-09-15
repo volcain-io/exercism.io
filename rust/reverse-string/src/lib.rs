cdbedf659e85b998a433b1a8afcd86b9f724065b
volcain-io <volcain.io@gmail.com>
2020-09-15 16:38:00 +0200

    Add Unicode support

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,7 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect::<String>()
+    UnicodeSegmentation::graphemes(input, true)
+        .rev()
+        .collect::<String>()
 }
