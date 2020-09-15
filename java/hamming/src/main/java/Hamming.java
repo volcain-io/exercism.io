08eb19d321824d4f36f6d3f32bec38358e4fa1d3
volcain-io <volcain.io@gmail.com>
2020-09-15 16:53:11 +0200

    Calculate Hamming Distance inside constructor

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -2,26 +2,21 @@ import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
 
 public class Hamming {
-  private String leftStrand;
-  private String rightStrand;
+  private int hammingDistance = 0;
 
-  public Hamming(final String _leftStrand, final String _rightStrand) {
-    this.validate(_leftStrand, _rightStrand);
-    this.leftStrand = _leftStrand;
-    this.rightStrand = _rightStrand;
+  public Hamming(final String leftStrand, final String rightStrand) {
+    this.validate(leftStrand, rightStrand);
+
+    CharacterIterator it = new StringCharacterIterator(leftStrand);
+    while (it.current() != CharacterIterator.DONE) {
+      if (it.current() != rightStrand.charAt(it.getIndex()))
+        this.hammingDistance++;
+      it.next();
+    }
   }
 
   public int getHammingDistance() {
-    int count = 0;
-
-    CharacterIterator it = new StringCharacterIterator(this.leftStrand);
-    while (it.current() != CharacterIterator.DONE) {
-      if (it.current() != this.rightStrand.charAt(it.getIndex()))
-        count++;
-      it.next();
-    }
-
-    return count;
+    return this.hammingDistance;
   }
 
   private void validate(final String _leftStrand, final String _rightStrand) {
