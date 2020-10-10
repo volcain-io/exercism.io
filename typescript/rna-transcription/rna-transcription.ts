4bdaa1fea978beec73bd0d989827d29ceb2fae60
volcain-io <volcain.io@gmail.com>
2020-10-10 16:25:19 +0200

    Exit early on failure
    Using a regular expression to check the given DNA strand, so we
    can exit early in the code. If the DNA strand is correct, we can
    convert it to its corresponding RNA.

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -4,16 +4,24 @@ const complements: Readonly<Record<string, string>> = {
     T: "A",
     A: "U",
 };
+const DNANucleotideList = (Object.keys(complements)).reduce((acc, curr) => "" + acc + curr);
+const re = new RegExp("[^" + DNANucleotideList + "]");
 
 class Transcriptor {
     toRna(dnaStrand: string): string {
+        this.checkForInvalidDNA(dnaStrand);
+
         return dnaStrand.split('')
-            .map((ch) => complements[ch] || this.raiseError())
+            .map((ch) => complements[ch])
             .join('');
     }
 
     private raiseError(): Error {
         throw new Error('Invalid input DNA.');
+    }
+
+    private checkForInvalidDNA(input: string): void {
+        input.match(re) !== null && this.raiseError();
     }
 }
 
