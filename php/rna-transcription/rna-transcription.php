4558afedefe18edc73478542a0df8309a6e6c10d
volcain-io <volcain.io@gmail.com>
2020-10-15 17:13:20 +0200

    Use php function 'strtr'

--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -4,12 +4,5 @@ define("DNA_TO_RNA_MAP", ['G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U']);
 
 function toRna(string $dna): string
 {
-  $arr = str_split($dna);
-  $rna = array_map('doCheck', $arr);
-  return join('', $rna);
-}
-
-function doCheck(string $ch): string
-{
-  return DNA_TO_RNA_MAP[$ch];
+  return strtr($dna, DNA_TO_RNA_MAP);
 }
