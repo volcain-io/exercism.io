49c8cd12b6ffc6e51787d2bbb3fb0be51b1c801c
volcain-io <volcain.io@gmail.com>
2020-11-17 15:49:28 +0100

    Rename function parameter

--- php/grains/grains.php
+++ php/grains/grains.php
@@ -6,18 +6,18 @@ define("LAST_SQUARE", 64);
 /**
  * Calculate number of grains on given square.
  *
- * @param int $num The square to calculate the grains for.
+ * @param int $exponent The square to calculate the grains for.
  *
  * @throws InvalidArgumentException If $num is not between 1 and 64.
  *
  * @return string The result as a string.
  */
-function square(int $num): string
+function square(int $exponent): string
 {
-  if ($num >= FIRST_SQUARE && $num <= LAST_SQUARE)
-    return bcpow(2, $num - 1);
+  if ($exponent >= FIRST_SQUARE && $exponent <= LAST_SQUARE)
+    return bcpow(2, $exponent - 1);
 
-  throw new InvalidArgumentException("Input must be between " . FIRST_SQUARE . "  and " . LAST_SQUARE . ", but was ${num}.");
+  throw new InvalidArgumentException("Input must be between " . FIRST_SQUARE . "  and " . LAST_SQUARE . ", but was ${exponent}.");
 }
 
 /**
