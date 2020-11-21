4d56e43b99bf4d000b45b9efa8e2ee05cd203351
volcain-io <volcain.io@gmail.com>
2020-11-21 12:28:33 +0100

    Initial commit

---
+++ php/binary/binary.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Convert the given binary number, represented as a string, to its decimal equivalent using first principles.
+ *
+ * @param string $binary The string to convert.
+ *
+ * @return int The result as int.
+ *
+ * @throws InvalidArgumentException If given string is not a valid binary string.
+ */
+function parse_binary(string $binary = ""): int
+{
+  if (!isValidBinary($binary)) {
+    throw new InvalidArgumentException("Invalid input. Only 0's and 1's allowed.");
+  }
+
+  $decimal = 0;
+  $rev = strrev($binary);
+  $idx = strlen($rev) - 1;
+  while (0 <= $idx) {
+    $decimal += $rev[$idx] * (2 ** $idx);
+    $idx--;
+  }
+  return $decimal;
+}
+
+/**
+ * Validate, that given string is a valid binary number.
+ *
+ * @param string $binary The string to validate.
+ *
+ * @return `true` on success, `false` on failure.
+ */
+function isValidBinary(string $binary = ""): bool
+{
+  return preg_match('/^[01]+$/u', $binary);
+}
