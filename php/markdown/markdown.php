8d143ace9141d3899d7246498f14f99407505db0
volcain-io <volcain.io@gmail.com>
2020-11-25 16:34:47 +0100

    Create functions for each duplicate code
    We now have following functions to make the code more readable:
    - createHeaders
    - createBold
    - createItalic
    - createParagraph
    - createUnorderedListItem
    - createUnorderedList

--- php/markdown/markdown.php
+++ php/markdown/markdown.php
@@ -4,66 +4,19 @@ function parseMarkdown($markdown)
 {
   $lines = explode("\n", $markdown);
 
-  $isInList = false;
-
   foreach ($lines as &$line) {
-    parseHeaders($line);
-
-    if (preg_match('/\*(.*)/', $line, $matches)) {
-      $isBold = false;
-      $isItalic = false;
-      if (preg_match('/(.*)__(.*)__(.*)/', $matches[1], $matches2)) {
-        $matches[1] = $matches2[1] . '<em>' . $matches2[2] . '</em>' . $matches2[3];
-        $isBold = true;
-      }
-
-      if (preg_match('/(.*)_(.*)_(.*)/', $matches[1], $matches3)) {
-        $matches[1] = $matches3[1] . '<i>' . $matches3[2] . '</i>' . $matches3[3];
-        $isItalic = true;
-      }
-
-      if (!$isInList) {
-        $isInList = true;
-
-        if ($isItalic || $isBold) {
-          $line = "<ul><li>" . trim($matches[1]) . "</li>";
-        } else {
-          $line = "<ul><li><p>" . trim($matches[1]) . "</p></li>";
-        }
-      } else {
-        if ($isItalic || $isBold) {
-          $line = "<li>" . trim($matches[1]) . "</li>";
-        } else {
-          $line = "<li><p>" . trim($matches[1]) . "</p></li>";
-        }
-      }
-    } else {
-      if ($isInList) {
-        $line = "</ul>" . $line;
-        $isInList = false;
-      }
-    }
-
-    if (!preg_match('/<h|<ul|<p|<li/', $line)) {
-      $line = "<p>$line</p>";
-    }
-
-    if (preg_match('/(.*)__(.*)__(.*)/', $line, $matches)) {
-      $line = $matches[1] . '<em>' . $matches[2] . '</em>' . $matches[3];
-    }
-
-    if (preg_match('/(.*)_(.*)_(.*)/', $line, $matches)) {
-      $line = $matches[1] . '<i>' . $matches[2] . '</i>' . $matches[3];
-    }
+    createHeaders($line);
+    createUnorderedListItem($line);
+    createParagraph($line);
+    createBold($line);
+    createItalic($line);
   }
-  $html = join($lines);
-  if ($isInList) {
-    $html .= '</ul>';
-  }
-  return $html;
+  createUnorderedList($lines);
+
+  return join($lines);
 }
 
-function parseHeaders(string &$line = '')
+function createHeaders(string &$line = '')
 {
   if (preg_match("/^######(.*)/", $line, $matches)) {
     $line = "<h6>" . trim($matches[1]) . "</h6>";
@@ -72,4 +25,64 @@ function parseHeaders(string &$line = '')
   } elseif (preg_match("/^#(.*)/", $line, $matches)) {
     $line = "<h1>" . trim($matches[1]) . "</h1>";
   }
+}
+
+function createBold(string &$line): bool
+{
+  if (preg_match('/(.*)__(.*)__(.*)/', $line, $matches)) {
+    $line = $matches[1] . '<em>' . $matches[2] . '</em>' . $matches[3];
+    return true;
+  }
+  return false;
+}
+
+function createItalic(string &$line): bool
+{
+  if (preg_match('/(.*)_(.*)_(.*)/', $line, $matches)) {
+    $line = $matches[1] . '<i>' . $matches[2] . '</i>' . $matches[3];
+    return true;
+  }
+  return false;
+}
+
+function createParagraph(string &$line): bool
+{
+  if (!preg_match('/<h|<ul|<p|<li/', $line)) {
+    $line = "<p>$line</p>";
+    return true;
+  }
+  return false;
+}
+
+function createUnorderedListItem(string &$line): bool
+{
+  if (preg_match('/\*(.*)/', $line, $matches)) {
+    $isBold = createBold($matches[1]);
+    $isItalic = createItalic($matches[1]);
+
+    if ($isItalic || $isBold) {
+      $line = "<li>" . trim($matches[1]) . "</li>";
+    } else {
+      $line = "<li><p>" . trim($matches[1]) . "</p></li>";
+    }
+    return true;
+  }
+  return false;
+}
+
+function createUnorderedList(array &$lines): bool
+{
+  $unorderedList = array_filter($lines, function ($val) {
+    $start = substr($val, 0, 4);
+    $end = substr($val, -5);
+    if ($start === '<li>' && $end === '</li>')
+      return $val;
+  });
+  if (count($unorderedList) > 0) {
+    $unorderedList[array_key_first($unorderedList)] = '<ul>' . $unorderedList[array_key_first($unorderedList)];
+    $unorderedList[array_key_last($unorderedList)] = $unorderedList[array_key_last($unorderedList)] . '</ul>';
+    $lines = array_replace($lines, $unorderedList);
+    return true;
+  }
+  return false;
 }
