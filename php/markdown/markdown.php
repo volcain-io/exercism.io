21ed19a98a8291ba4b4b644af95ecf3cb97d15ea
volcain-io <volcain.io@gmail.com>
2020-11-25 15:14:19 +0100

    Add function parseHeaders, Remove duplicate code

--- php/markdown/markdown.php
+++ php/markdown/markdown.php
@@ -7,28 +7,23 @@ function parseMarkdown($markdown)
   $isInList = false;
 
   foreach ($lines as &$line) {
-    if (preg_match('/^######(.*)/', $line, $matches)) {
-      $line = "<h6>" . trim($matches[1]) . "</h6>";
-    } elseif (preg_match('/^##(.*)/', $line, $matches)) {
-      $line = "<h2>" . trim($matches[1]) . "</h2>";
-    } elseif (preg_match('/^#(.*)/', $line, $matches)) {
-      $line = "<h1>" . trim($matches[1]) . "</h1>";
-    }
+    parseHeaders($line);
 
     if (preg_match('/\*(.*)/', $line, $matches)) {
+      $isBold = false;
+      $isItalic = false;
+      if (preg_match('/(.*)__(.*)__(.*)/', $matches[1], $matches2)) {
+        $matches[1] = $matches2[1] . '<em>' . $matches2[2] . '</em>' . $matches2[3];
+        $isBold = true;
+      }
+
+      if (preg_match('/(.*)_(.*)_(.*)/', $matches[1], $matches3)) {
+        $matches[1] = $matches3[1] . '<i>' . $matches3[2] . '</i>' . $matches3[3];
+        $isItalic = true;
+      }
+
       if (!$isInList) {
         $isInList = true;
-        $isBold = false;
-        $isItalic = false;
-        if (preg_match('/(.*)__(.*)__(.*)/', $matches[1], $matches2)) {
-          $matches[1] = $matches2[1] . '<em>' . $matches2[2] . '</em>' . $matches2[3];
-          $isBold = true;
-        }
-
-        if (preg_match('/(.*)_(.*)_(.*)/', $matches[1], $matches3)) {
-          $matches[1] = $matches3[1] . '<i>' . $matches3[2] . '</i>' . $matches3[3];
-          $isItalic = true;
-        }
 
         if ($isItalic || $isBold) {
           $line = "<ul><li>" . trim($matches[1]) . "</li>";
@@ -36,18 +31,6 @@ function parseMarkdown($markdown)
           $line = "<ul><li><p>" . trim($matches[1]) . "</p></li>";
         }
       } else {
-        $isBold = false;
-        $isItalic = false;
-        if (preg_match('/(.*)__(.*)__(.*)/', $matches[1], $matches2)) {
-          $matches[1] = $matches2[1] . '<em>' . $matches2[2] . '</em>' . $matches2[3];
-          $isBold = true;
-        }
-
-        if (preg_match('/(.*)_(.*)_(.*)/', $matches[1], $matches3)) {
-          $matches[1] = $matches3[1] . '<i>' . $matches3[2] . '</i>' . $matches3[3];
-          $isItalic = true;
-        }
-
         if ($isItalic || $isBold) {
           $line = "<li>" . trim($matches[1]) . "</li>";
         } else {
@@ -78,4 +61,15 @@ function parseMarkdown($markdown)
     $html .= '</ul>';
   }
   return $html;
+}
+
+function parseHeaders(string &$line = '')
+{
+  if (preg_match("/^######(.*)/", $line, $matches)) {
+    $line = "<h6>" . trim($matches[1]) . "</h6>";
+  } elseif (preg_match("/^##(.*)/", $line, $matches)) {
+    $line = "<h2>" . trim($matches[1]) . "</h2>";
+  } elseif (preg_match("/^#(.*)/", $line, $matches)) {
+    $line = "<h1>" . trim($matches[1]) . "</h1>";
+  }
 }
