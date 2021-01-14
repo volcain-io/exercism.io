2fc13b8012fb3f3d0332f2a2649b920deaaddd5e
volcain-io <volcain.io@gmail.com>
2021-01-14 21:49:02 +0100

    Add imperative solution

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,6 +1,24 @@
+// imperative solution
 export function compute(leftStrand = '', rightStrand = '') {
   if (leftStrand.length === rightStrand.length) {
-    if (leftStrand !== rightStrand) return leftStrand.split('').filter((ch, idx) => ch !== rightStrand[idx]).length;
+    let diffCount = 0;
+    if (leftStrand !== rightStrand) {
+      const splitted = leftStrand.split('');
+      for(let idx = 0; idx < splitted.length; idx++) {
+        const ch = splitted[idx];
+        if ( ch !== rightStrand[idx] )
+          diffCount++;
+      }
+    }
+    return diffCount;
+  } throw new Error('left and right strands must be of equal length');
+}
+
+// declarative solution
+export function computeDeclarative(leftStrand = '', rightStrand = '') {
+  if (leftStrand.length === rightStrand.length) {
+    if (leftStrand !== rightStrand)
+      return leftStrand.split('').filter((ch, idx) => ch !== rightStrand[idx]).length;
     return 0;
   } throw new Error('left and right strands must be of equal length');
 }
