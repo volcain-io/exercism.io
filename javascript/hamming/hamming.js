6ae1b4f0fbe5807c19941295c4726c066654de93
volcain-io <volcain.io@gmail.com>
2021-01-16 12:59:47 +0100

    Make imperative code more readable

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,24 +1,30 @@
 // imperative solution
-export function compute(leftStrand = '', rightStrand = '') {
-  if (leftStrand.length === rightStrand.length) {
-    let diffCount = 0;
-    if (leftStrand !== rightStrand) {
-      const splitted = leftStrand.split('');
-      for(let idx = 0; idx < splitted.length; idx++) {
-        const ch = splitted[idx];
-        if ( ch !== rightStrand[idx] )
-          diffCount++;
-      }
+function compute(leftStrand = '', rightStrand = '') {
+  if (leftStrand.length !== rightStrand.length) {
+    throw new Error('left and right strands must be of equal length');
+  }
+
+  let diffCount = 0;
+  if (leftStrand === rightStrand) return diffCount;
+
+  for (let idx = 0; idx < leftStrand.length; idx += 1) {
+    if (leftStrand.charAt(idx) !== rightStrand.charAt(idx)) {
+      diffCount += 1;
     }
-    return diffCount;
-  } throw new Error('left and right strands must be of equal length');
+  }
+  return diffCount;
 }
 
 // declarative solution
-export function computeDeclarative(leftStrand = '', rightStrand = '') {
+function computeDeclarative(leftStrand = '', rightStrand = '') {
   if (leftStrand.length === rightStrand.length) {
-    if (leftStrand !== rightStrand)
-      return leftStrand.split('').filter((ch, idx) => ch !== rightStrand[idx]).length;
+    if (leftStrand !== rightStrand) {
+      return leftStrand.split('').filter((ch, idx) => ch !== rightStrand[idx])
+        .length;
+    }
     return 0;
-  } throw new Error('left and right strands must be of equal length');
+  }
+  throw new Error('left and right strands must be of equal length');
 }
+
+export { compute, computeDeclarative };
