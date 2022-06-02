ec057a7875bdc7ec9c7bdef1bf9214e2c3c3e822
volcain-io <volcain.io@gmail.com>
2022-06-02 20:46:24 +0200

    Ignore vscode settings

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,40 +1,22 @@
 class Lasagna
 {
-    const int EXPECTED_BAKE_TIME_IN_MINUTES = 40;
-    const int PREPARATION_TIME_PER_LAYER_IN_MINUTES = 2;
+    private const int EXPECTED_BAKE_TIME_IN_MINUTES = 40;
+    private const int PREPARATION_TIME_PER_LAYER_IN_MINUTES = 2;
 
-    public int ExpectedMinutesInOven()
-    {
-        return EXPECTED_BAKE_TIME_IN_MINUTES;
-    }
+    public int ExpectedMinutesInOven() => EXPECTED_BAKE_TIME_IN_MINUTES;
 
     public int RemainingMinutesInOven(int elapsedMinutesInOven)
     {
-        if (elapsedMinutesInOven < 0 || elapsedMinutesInOven > int.MaxValue)
-        {
-            return 0;
-        }
-
-        if (elapsedMinutesInOven > this.ExpectedMinutesInOven())
-        {
-            return 0;
-        }
-        
         return this.ExpectedMinutesInOven() - elapsedMinutesInOven;
     }
 
     public int PreparationTimeInMinutes(int numberOfLayers)
     {
-        if (numberOfLayers < 0 || numberOfLayers > int.MaxValue)
-        {
-            return 0;
-        }
-
         return numberOfLayers * PREPARATION_TIME_PER_LAYER_IN_MINUTES;
     }
 
     public int ElapsedTimeInMinutes(int numberOfLayers, int elapsedMinutesInOven)
     {
-        return this.ExpectedMinutesInOven() - this.RemainingMinutesInOven(elapsedMinutesInOven) + this.PreparationTimeInMinutes(numberOfLayers);
+        return this.PreparationTimeInMinutes(numberOfLayers) + elapsedMinutesInOven;
     }
 }
