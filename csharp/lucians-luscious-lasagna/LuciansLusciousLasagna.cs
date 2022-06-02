86efede76ca040e84c413103b030c76ec8afef94
volcain-io <volcain.io@gmail.com>
2022-06-02 20:48:44 +0200

    Revert "Ignore vscode settings"
    This reverts commit 1c8d508bc7259a972adcc59432911b0d6b9982da.

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,22 +1,40 @@
 class Lasagna
 {
-    private const int EXPECTED_BAKE_TIME_IN_MINUTES = 40;
-    private const int PREPARATION_TIME_PER_LAYER_IN_MINUTES = 2;
+    const int EXPECTED_BAKE_TIME_IN_MINUTES = 40;
+    const int PREPARATION_TIME_PER_LAYER_IN_MINUTES = 2;
 
-    public int ExpectedMinutesInOven() => EXPECTED_BAKE_TIME_IN_MINUTES;
+    public int ExpectedMinutesInOven()
+    {
+        return EXPECTED_BAKE_TIME_IN_MINUTES;
+    }
 
     public int RemainingMinutesInOven(int elapsedMinutesInOven)
     {
+        if (elapsedMinutesInOven < 0 || elapsedMinutesInOven > int.MaxValue)
+        {
+            return 0;
+        }
+
+        if (elapsedMinutesInOven > this.ExpectedMinutesInOven())
+        {
+            return 0;
+        }
+        
         return this.ExpectedMinutesInOven() - elapsedMinutesInOven;
     }
 
     public int PreparationTimeInMinutes(int numberOfLayers)
     {
+        if (numberOfLayers < 0 || numberOfLayers > int.MaxValue)
+        {
+            return 0;
+        }
+
         return numberOfLayers * PREPARATION_TIME_PER_LAYER_IN_MINUTES;
     }
 
     public int ElapsedTimeInMinutes(int numberOfLayers, int elapsedMinutesInOven)
     {
-        return this.PreparationTimeInMinutes(numberOfLayers) + elapsedMinutesInOven;
+        return this.ExpectedMinutesInOven() - this.RemainingMinutesInOven(elapsedMinutesInOven) + this.PreparationTimeInMinutes(numberOfLayers);
     }
 }
